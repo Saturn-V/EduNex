7fb1d1ecba8ab44838dd81363973542d0d70f11b
MrCodeCollector <alexaaronpena@gmail.com>
2015-10-04 21:01:24 -0700

    JS
    Moved JS code from html to js file.

--- js/initialize.js
+++ js/initialize.js
@@ -6,3 +6,28 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+//initialize Parse
+ Parse.initialize("cVPrk4Mea6My6Jc0HJQwcDKqCxAzAcKLmL1iukqQ","bEIUTjLL3VHn3ceYeIL1Nevmdo0kTKjbD1FiCqzx");
+
+// Generate a User Account
+var user = new Parse.User();
+user.set("username", document.getElementById("my_id").value);
+user.set("password", document.getElementById("my_pass").value);
+user.set("email", document.getElementById("my_email").value);
+
+// other fields can be set just like with Parse.Object
+user.set("phone", document.getElementById("my_phone").value);
+user.set("firstName", document.getElementById("my_first_name").value);
+user.set("lastName", document.getElementById("my_last_name").value);
+
+user.signUp(null, {
+      success: function(user) {
+             // Hooray! Let them use the app now.
+             console.log("So far so good!");
+       },
+       error: function(user, error) {
+             // Show the error message somewhere and let the user try again.
+             alert("Error: " + error.code + " " + error.message);
+       }
+ });
