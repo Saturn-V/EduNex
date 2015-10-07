5ad0fbc723185af765057f4be0e03cd6a57eaa0c
MrCodeCollector <alexaaronpena@gmail.com>
2015-10-06 22:12:32 -0700

    Update
    Update release files

--- js/initialize.js
+++ js/initialize.js
@@ -1,35 +1,54 @@
-//initialize Parse
+ //initialize Parse
  Parse.initialize("cVPrk4Mea6My6Jc0HJQwcDKqCxAzAcKLmL1iukqQ","bEIUTjLL3VHn3ceYeIL1Nevmdo0kTKjbD1FiCqzx");
 
-//Materialize CSS stuff
-(function($){
-  $(function(){
+ //Materialize CSS stuff
+ (function($){
+       $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.collapsible').collapsible();
+             $('.button-collapse').sideNav();
+             $('.collapsible').collapsible();
 
-  }); // end of document ready
+             //parse stuff
+             // Generate a User Account
+             var username = $("#my_id").val();
+
+             var password = $("#my_password").val();
+
+             var email = $("#my_email").val();
+
+             var firstname = $("#first_name").val();
+
+             var lastname = $("#last_name").val();
+
+             var user = new Parse.User();
+  
+             // other fields can be set just like with Parse.Object
+             //user.set("phone", "650-555-0000");
+              
+             $("#sign_up_submit").click( function(event) {
+                   console.log("You clicked me!");
+                   event.preventDefault();
+
+                   user.set("username", username);
+                   user.set("password", password);
+                   user.set("email", email);
+                   user.set("firstname", firstname);
+                   user.set("lastname", lastname);
+
+                   user.signUp(null, {
+                         success: function(user) {
+                               // Hooray! Let them use the app now.
+                               alert("yay! it worked (created user)");
+                               console.log("This works!");
+                         },
+                         error: function(user, error) {
+                               // Show the error message somewhere and let the user try again.
+                               alert("Error: " + error.code + " " + error.message);
+                               console.log("Something is wrong...");
+                         }
+                   });
+             });
+
+
+       }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-//PARSE STUFF
-// Generate a User Account
-var user = new Parse.User();
-user.set("username", document.getElementById("my_id").value);
-user.set("password", document.getElementById("my_pass").value);
-user.set("email", document.getElementById("my_email").value);
-
-// other fields can be set just like with Parse.Object
-user.set("phone", document.getElementById("my_phone").value);
-user.set("firstName", document.getElementById("my_first_name").value);
-user.set("lastName", document.getElementById("my_last_name").value);
-
-user.signUp(null, {
-      success: function(user) {
-             // Hooray! Let them use the app now.
-             console.log("So far so good!");
-       },
-       error: function(user, error) {
-             // Show the error message somewhere and let the user try again.
-             alert("Error: " + error.code + " " + error.message);
-       }
- });
